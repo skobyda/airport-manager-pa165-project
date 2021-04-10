34bd4773241a584ae79f67568948b20972b09c24
Jozef Vanický <506486@mail.muni.cz>
2021-04-11 01:49:25 +0200

    Revert "Merge branch 'develop' into 'master'"
    This reverts merge request !28

---
+++ src/main/java/cz/fi/muni/pa165/project/Main.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.pa165.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
